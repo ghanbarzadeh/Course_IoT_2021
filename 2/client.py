68016c1aab4f84acd256331f09d387faf3acb982
ghanbarzadeh <agz1986@gmail.com>
2022-01-21 20:33:08 +0330

    added client-server code

--- 2/client.py
+++ 2/client.py
@@ -2,36 +2,50 @@ import socket
 import pickle as pkl
 import time
 from train_head_model import train_model
+import os
     
 HEADERSIZE = 10
+WEIGHTS_FOLDER = 'models'
 
-IP = "192.168.0.1"
+IP = "127.0.0.1"
 PORT = 1234
 
+print('---------------------Starting Client---------------------')
+print('- Connecting to server on IP: {}, Port: {}'.format(IP, PORT))
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((IP, PORT))
 
-# get weight and version
 message_header = s.recv(HEADERSIZE)
 message_length = int(message_header.decode('utf-8').strip())
 version = s.recv(message_length).decode('utf-8')
 version = int(version)
-print('Getting weights version: {}'.format(version))
+print('- Getting weights version: {}'.format(version))
 
-with open("weights_{:04d}.h5".format(version),'wb') as file:
+with open(os.path.join(WEIGHTS_FOLDER, "weights_{:04d}.h5".format(version)),'wb') as file:
     message_header = s.recv(HEADERSIZE)
     message_length = int(message_header.decode('utf-8').strip())
     weights = s.recv(message_length)
     file.write(weights)
-print('Done!')
+print(' - Weights received')
+print('  -- Saving weights to file: {}'.format(os.path.join(WEIGHTS_FOLDER, "weights_{:04d}.h5".format(version))))
 
-print('training')
-train_model('weights_{:04d}.h5'.format(version), version)
+print(' - Training with latest weights')
+train_model(weights="weights_{:04d}.h5".format(version))
 
-print('Sending weights back to server')
-filename = "weights_{:04d}.h5".format(version+1)
+print(' - Sending weights version {} back to server'.format(version+1))
+filename = os.path.join(WEIGHTS_FOLDER, "weights_{:04d}.h5".format(version+1))
 with open(filename, 'rb') as file:
     sendfile = file.read()
 sendfile = bytes(f"{len(sendfile):<{HEADERSIZE}}",'utf-8')+sendfile
 s.send(sendfile)
-print('Done!')
+print(' - Weights sent')
+
+print('- Getting new weights version: {}'.format(version+1))
+
+with open(os.path.join(WEIGHTS_FOLDER, "weights_{:04d}.h5".format(version+1)),'wb') as file:
+    message_header = s.recv(HEADERSIZE)
+    message_length = int(message_header.decode('utf-8').strip())
+    weights = s.recv(message_length)
+    file.write(weights)
+print(' - New weights received')
+print('  -- Saving weights to file: {}'.format(os.path.join(WEIGHTS_FOLDER, "weights_{:04d}.h5".format(version+1))))
--- 2/server.py
+++ 2/server.py
@@ -1,42 +1,89 @@
 import socket
 import os
 import glob
+from utils import *
 
-IP = "192.168.0.1"
+
+IP = "127.0.0.1"
 PORT = 1234
+HEADERSIZE = 10
 
+NUMBER_OF_CLIENTS = 2
 WEIGHTS_FOLDER = 'server_weights'
 
-weight_files = glob.glob(os.path.join(WEIGHTS_FOLDER, '/*.h5'))
+weight_files = glob.glob(os.path.join(WEIGHTS_FOLDER, '*.h5'))
 weight_files.sort()
 weight_file = os.path.split(weight_files[-1])[-1]
 weight_version = int(weight_file[8:12])
 
-HEADERSIZE = 10
-
+print('---------------------Starting Server---------------------')
+print('IP: {}, Port: {}'.format(IP, PORT))
+print('- Waiting for {} client(s)'.format(NUMBER_OF_CLIENTS))
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((IP, PORT))
 s.listen(5)
 
-clientsocket, address = s.accept()
-print(f"Connection from {address} has been established.")
-print('Sending weights version: {}, file: {}'.format(weight_version, weight_file))
-msg = str(weight_version).encode('utf-8')
-msg = bytes(f"{len(msg):<{HEADERSIZE}}", 'utf-8')+msg
-clientsocket.send(msg)
 
-filename = os.path.join(WEIGHTS_FOLDER, weight_file)
-with open(filename, 'rb') as file:
-    sendfile = file.read()
-sendfile = bytes(f"{len(sendfile):<{HEADERSIZE}}",'utf-8')+sendfile
-clientsocket.send(sendfile)
-print('Done!')
+clients = []
+for i in range(NUMBER_OF_CLIENTS):
+    clientsocket, address = s.accept()
+    clients.append((clientsocket, address))
+    print(f"  -- Connection from {address} has been established.")
+    print('    -- Sending weights version: {} to client# {}'.format(weight_version, i))
+    msg = str(weight_version).encode('utf-8')
+    msg = bytes(f"{len(msg):<{HEADERSIZE}}", 'utf-8')+msg
+    clientsocket.send(msg)
 
-print('getting new weights')
-with open("weights_{:04d}.pkl".format(weight_version+1),'wb') as file:
-    message_header = clientsocket.recv(HEADERSIZE)
-    message_length = int(message_header.decode('utf-8').strip())
-    weights = clientsocket.recv(message_length)
-    file.write(weights)
-print('Done!')
+    filename = os.path.join(WEIGHTS_FOLDER, weight_file)
+    with open(filename, 'rb') as file:
+        sendfile = file.read()
+    sendfile = bytes(f"{len(sendfile):<{HEADERSIZE}}",'utf-8')+sendfile
+    clientsocket.send(sendfile)
+    print('    -- Done')
+    print('')
+
+print('')
+print('- Waiting for new weights')
+for i in range(NUMBER_OF_CLIENTS):
+    clientsocket, address = clients[i]
+    new_weights_filename = os.path.join(WEIGHTS_FOLDER, "client_{}_weights_{:04d}.h5".format(i, weight_version+1))
+    with open(new_weights_filename,'wb') as file:
+        message_header = clientsocket.recv(HEADERSIZE)
+        message_length = int(message_header.decode('utf-8').strip())
+        weights = clientsocket.recv(message_length)
+        file.write(weights)
+    print('    -- Received weights version: {} from client# {}'.format(weight_version+1, i))
+    print('     --- Saved to file {}'.format(new_weights_filename))
+    print('')
+
+print('')
+print('- Averaging the weights from clients')
+
+all_weights = os.listdir(WEIGHTS_FOLDER)
+client_weights = []
+for file in all_weights:
+    if file[0] == 'c':
+        client_weights.append(file)
+client_weights = [os.path.join(WEIGHTS_FOLDER, cw) for cw in client_weights]
+
+save_path = os.path.join(WEIGHTS_FOLDER, "weights_{:04d}.h5".format(weight_version+1))
+new_weights = average_weights(client_weights, save_path)
+
+for file in client_weights:
+    os.remove(file)
+
+print('- Sending new weights version {} to clients'.format(weight_version+1))
+
+weights_file = os.path.join(WEIGHTS_FOLDER, "weights_{:04d}.h5".format(weight_version+1))
+
+for i in range(NUMBER_OF_CLIENTS):
+    clientsocket, address = clients[i]
+    print('    -- Sending weights version: {} to client# {}'.format(weight_version+1, i))
+    with open(weights_file, 'rb') as file:
+        sendfile = file.read()
+    sendfile = bytes(f"{len(sendfile):<{HEADERSIZE}}",'utf-8')+sendfile
+    clientsocket.send(sendfile)
+    print('    -- Done')
+    print('')
+
